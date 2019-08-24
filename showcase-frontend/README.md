f3db1540f363f4a95fa719ed6a1c71acbfb819ba
kreuzerk <kevin.kreuzer90@icloud.com>
2019-08-24 08:49:30 +0200

    feat(showcase): adjust setup

--- showcase-frontend/README.md
+++ showcase-frontend/README.md
@@ -30,12 +30,14 @@ To get more help on the Angular CLI use `ng help` or go check out the [Angular C
 
 ### HTML snippet
 ```
+<div class="row">
 <div class="players" *ngFor="let player of players">
       <img [src]="player.imgURL" class="card-img-top" alt="...">
       <div class="card-body">
         <h3 class="card-title">{{ player.name }}</h3>
         <p class="card-text">{{ player.info }} </p>
       </div>
+</div>
 </div>
  ```
 
--- showcase-frontend/src/app/app.module.ts
+++ showcase-frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {NavComponent} from './nav/nav.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {NavComponent} from './nav/nav.component';
     NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
